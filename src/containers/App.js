cd5126338fc5e78ccce8ce064349e6a33564dc5a
Hector Longarte <hector.longarte@gmail.com>
2021-07-22 10:47:38 +1000

    Added Back to top button

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useMemo }  from "react";
-import MyAppBar from "../components/toolbar/MyAppBar";
+import PropTypes from 'prop-types';
 import Header from "../components/header/Header";
 import About from "../components/about/About";
 import Resume from "../components/resume/Resume";
@@ -7,14 +7,65 @@ import Portfolio from "../components/portfolio/Portfolio";
 import Tech from "../components/tech/Tech";
 // import MandalorianFont from "../static/fonts/mandalorelasertitle.ttf";
 
+import { makeStyles } from '@material-ui/core/styles';
 import {
   createTheme,
   ThemeProvider,
   responsiveFontSizes,
 } from "@material-ui/core/styles";
+import Zoom from '@material-ui/core/Zoom';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Fab from '@material-ui/core/Fab';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    color:"lightblue",
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+function ScrollTop(props) {
+  const { children, window } = props;
+  const classes = useStyles();
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
+}
+
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
 
 function App(props) {
   const homeRef = useRef(null);
@@ -61,11 +112,18 @@ function App(props) {
       <ThemeProvider theme={theme}>
         <CssBaseline />
           {/* <MyAppBar references={references}/> */}
-          <Header references={references}/>
+          <section id="back-to-top-anchor" >
+            <Header references={references}/>
+          </section>
           <About reference={aboutRef}/>
           <Resume reference={resumeRef}/>
           <Tech reference={techRef}/>
           <Portfolio reference={portfolioRef}/>
+          <ScrollTop {...props}>
+            <Fab color="blue" size="large" aria-label="Back to top">
+              <KeyboardArrowUpIcon />
+            </Fab>
+          </ScrollTop>
       </ThemeProvider>
     </React.Fragment>
   );
